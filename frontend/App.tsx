15016f5bfe811dfb85cc2cecf7904c9b1664341e
Jacob Cambell <jacob@jacobcambell.com>
2021-10-10 16:41:36 -0600

    Hide header in Stack Navigator

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Lander from './src/Lander';
+import Main from './src/Main';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="Lander" component={Lander} />
             </Stack.Navigator>
         </NavigationContainer>
