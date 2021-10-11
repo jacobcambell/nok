86e63f03bd6567885f4f2894ff8dea0acdcdbf45
Jacob Cambell <jacob@jacobcambell.com>
2021-10-10 21:19:47 -0600

    Disable transition animations

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -3,15 +3,17 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Lander from './src/Lander';
 import Login from './src/pages/Login';
+import Register from './src/pages/Register';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Navigator screenOptions={{ headerShown: false, animation: 'none' }}>
                 <Stack.Screen name="Lander" component={Lander} />
                 <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="Register" component={Register} />
             </Stack.Navigator>
         </NavigationContainer>
     );
