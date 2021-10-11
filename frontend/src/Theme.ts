f1e127a49ab8e714065130fb29d774616aa5ba8c
Jacob Cambell <jacob@jacobcambell.com>
2021-10-10 20:29:33 -0600

    Initial Theme file

---
+++ frontend/src/Theme.ts
@@ -0,0 +1,15 @@
+export const Theme = {
+    colors: {
+        grey: '#545E75',
+        darkblue: '#191D2A',
+        mediumblue: '#2E74B7',
+        lightblue: '#95D3E9',
+        highlight: '#EFD602',
+        white: '#FFFFFF'
+    },
+    fontSizes: {
+        small: 16,
+        medium: 22,
+        large: 28
+    }
+}
