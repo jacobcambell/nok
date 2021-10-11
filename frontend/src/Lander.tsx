ccb880c8c03f70ffb2b306d60a1133ff0df7db43
Jacob Cambell <jacob@jacobcambell.com>
2021-10-10 20:29:17 -0600

    Initial Lander design

--- frontend/src/Lander.tsx
+++ frontend/src/Lander.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Text, StyleSheet, View, Pressable } from "react-native";
+import { Theme } from "./Theme";
 
 const Lander = () => {
     return (
         <View style={styles.content}>
-            <Text>Welcome to Nok</Text>
-            <Pressable>
-                <Text>Sign Up</Text>
+            <Text style={styles.header}>Welcome to Nok</Text>
+
+            <Pressable style={[styles.button, styles.signUp]}>
+                <Text style={styles.buttonText}>Sign Up</Text>
             </Pressable>
-            <Pressable>
-                <Text>Log In</Text>
+            <Pressable style={[styles.button, styles.login]}>
+                <Text style={styles.buttonText}>Login</Text>
             </Pressable>
         </View>
     );
@@ -19,7 +21,29 @@ const styles = StyleSheet.create({
     content: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        padding: 50
+    },
+    header: {
+        fontSize: Theme.fontSizes.large,
+        paddingBottom: 25
+    },
+    button: {
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+        marginVertical: 5,
+        alignSelf: 'stretch',
+        alignItems: 'center'
+    },
+    buttonText: {
+        color: Theme.colors.white,
+        fontWeight: 'bold'
+    },
+    signUp: {
+        backgroundColor: Theme.colors.darkblue
+    },
+    login: {
+        backgroundColor: Theme.colors.mediumblue
     }
 });
 
