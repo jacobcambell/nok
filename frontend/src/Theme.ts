b9c9439615b0b7726294c39dd3f3ab0e488040e0
Jacob Cambell <jacob@jacobcambell.com>
2021-10-10 21:03:12 -0600

    Additions to Theme

--- frontend/src/Theme.ts
+++ frontend/src/Theme.ts
@@ -1,6 +1,8 @@
 export const Theme = {
     colors: {
+        black: '#000000',
         grey: '#545E75',
+        lightgrey: '#ced4da',
         darkblue: '#191D2A',
         mediumblue: '#2E74B7',
         lightblue: '#95D3E9',
@@ -8,8 +10,9 @@ export const Theme = {
         white: '#FFFFFF'
     },
     fontSizes: {
-        small: 16,
+        normal: 16,
         medium: 22,
         large: 28
-    }
+    },
+    borderRadius: 5
 }
