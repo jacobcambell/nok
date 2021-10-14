c29398ea483416bcb047333badb077cc35ea1218
Jacob Cambell <jacob@jacobcambell.com>
2021-10-14 09:25:52 -0600

    Small style changes

--- frontend/src/Center.tsx
+++ frontend/src/Center.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Theme } from './Theme';
 
-const Center = ({ children }: { children: any }) => {
-    return (
-        <View style={styles.center}>
-            {children}
-        </View>
-    );
+const Center = ({ children, vertical }: { children: any, vertical?: boolean }) => {
+    if (vertical === false) {
+        return (
+            <View style={styles.centerNoVertical}>
+                {children}
+            </View>
+        );
+    }
+    else {
+        return (
+            <View style={styles.center}>
+                {children}
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
@@ -15,8 +24,14 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: Theme.colors.darkblue
+        backgroundColor: Theme.colors.white
+    },
+    centerNoVertical: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: Theme.colors.white
     }
+
 });
 
 export default Center;
