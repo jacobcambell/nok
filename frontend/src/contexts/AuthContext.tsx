46d9f6094b53e2e1afef0d7363c3bbea4d160d2a
Jacob Cambell <jacob@jacobcambell.com>
2021-10-14 07:35:07 -0600

    Pass firebaseAuth through provider

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -2,23 +2,21 @@ import React, { createContext } from 'react';
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
-const firebaseConfig = {
+const firebaseApp = initializeApp({
     apiKey: "AIzaSyAnQ4G4n0kigRIap659em1tB3HnLUiL2I8",
     authDomain: "nokapp-2be53.firebaseapp.com",
     projectId: "nokapp-2be53",
     storageBucket: "nokapp-2be53.appspot.com",
     messagingSenderId: "752835443920",
     appId: "1:752835443920:web:a910db262baf63494472d5"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
+});
 
 export const AuthContext = createContext<any>(null);
+const firebaseAuth = getAuth();
 
 export default function AuthProvider({ children }: { children: any }) {
     return (
-        <AuthContext.Provider value={{ app, auth }}>
+        <AuthContext.Provider value={{ firebaseAuth }}>
             {children}
         </AuthContext.Provider>
     );
