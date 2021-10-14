a211bfa6fcb103fe0bcdccfd022ed57e29555761
Jacob Cambell <jacob@jacobcambell.com>
2021-10-14 09:44:16 -0600

    Initial backend setup

---
+++ backend/src/app.ts
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log('Listening on ' + PORT)
+})
