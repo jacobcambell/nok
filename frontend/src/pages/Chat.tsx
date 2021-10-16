a89d76720a5e0f52df226ca68eb563b3ec1798d8
Jacob Cambell <jacob@jacobcambell.com>
2021-10-16 14:48:52 -0600

    Show new message icon depending on is_read

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -6,11 +6,13 @@ import { Theme } from '../Theme'
 import { API_ENDPOINT } from '../EnvironmentVariables'
 import * as SecureStore from 'expo-secure-store';
 import axios from 'axios';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 interface MessageThread {
     id: number;
     username: string;
     msg_preview: string;
+    is_read: boolean;
 }
 
 export default function Chat({ navigation }: { navigation: any }) {
@@ -56,9 +58,16 @@ export default function Chat({ navigation }: { navigation: any }) {
                 {
                     messageThreads.length > 0 &&
                     messageThreads.map((thread, index) => (
-                        <Pressable onPress={() => { navigation.navigate('Conversation', { thread_id: thread.id, username: thread.username }) }} style={[index === messageThreads.length - 1 ? { borderBottomWidth: 0.5 } : { borderBottomWidth: 0 }, styles.thread]} key={thread.id}>
-                            <Text style={styles.threadUser}>{thread.username}</Text>
-                            <Text style={styles.threadMsg}>{thread.msg_preview}</Text>
+                        <Pressable
+                            onPress={() => { navigation.navigate('Conversation', { thread_id: thread.id, username: thread.username }) }}
+                            style={[index === messageThreads.length - 1 ? { borderBottomWidth: 0.5 } : { borderBottomWidth: 0 }, styles.thread, { backgroundColor: (thread.is_read) ? Theme.colors.white : Theme.colors.white }]}
+                            key={thread.id}
+                        >
+                            <Ionicons name={'ellipse'} size={12} style={{ color: Theme.colors.mediumblue, flex: 1, display: (thread.is_read) ? 'none' : 'flex' }} />
+                            <View style={{ flex: 7 }}>
+                                <Text style={styles.threadUser}>{thread.username}</Text>
+                                <Text style={styles.threadMsg}>{thread.msg_preview}</Text>
+                            </View>
                         </Pressable>
                     ))
                 }
@@ -89,7 +98,10 @@ const styles = StyleSheet.create({
         borderTopColor: Theme.colors.lightgrey,
         borderBottomColor: Theme.colors.lightgrey,
         paddingVertical: 5,
-        paddingHorizontal: 15
+        paddingHorizontal: 15,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
     },
     threadUser: {
         fontWeight: 'bold',
