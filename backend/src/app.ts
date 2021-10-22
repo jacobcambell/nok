1ad67dc40c2530e355a862ea91e25d0dae8e6721
Jacob Cambell <jacob@jacobcambell.com>
2021-10-21 22:20:19 -0600

    Use different charset for emojis

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,7 +18,8 @@ const con = mysql.createConnection({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    database: process.env.MYSQL_DATABASE,
+    charset: 'utf8mb4'
 });
 con.connect();
 
