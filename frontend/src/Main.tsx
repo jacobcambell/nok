978d4e0831161e3abc17c1c92d7876d75d0a396f
Jacob Cambell <jacob@jacobcambell.com>
2021-10-22 21:39:22 -0600

    Request push notification token from user

--- frontend/src/Main.tsx
+++ frontend/src/Main.tsx
@@ -9,6 +9,7 @@ import * as SecureStore from 'expo-secure-store';
 import axios from "axios";
 import { API_ENDPOINT } from './components/EnvironmentVariables';
 import { AuthContext } from "./contexts/AuthContext";
+import * as Notifications from 'expo-notifications'
 
 const Main = ({ navigation }: { navigation: any }) => {
 
@@ -20,17 +21,41 @@ const Main = ({ navigation }: { navigation: any }) => {
         // a ping to the server with the user object
 
         // Wait a few seconds for AuthContext to update the token
-        setTimeout(() => {
-            SecureStore.getItemAsync('firebase_idToken')
-                .then((idToken) => {
-                    // Ping the api with the user's idToken
-                    axios.post(`${API_ENDPOINT}/ping`, {
-                        idToken
-                    })
-                        .catch((err) => { })
+        setTimeout(async () => {
+            const idToken = await SecureStore.getItemAsync('firebase_idToken')
+
+            registerForPushNotificationsAsync().then((expoPushToken) => {
+                // Ping the api with the user's idToken
+                axios.post(`${API_ENDPOINT}/ping`, {
+                    idToken,
+                    expoPushToken
                 })
+                    .catch((err) => { })
+            })
         }, 3000);
     }, []);
+
+    async function registerForPushNotificationsAsync() {
+        let expoPushToken;
+
+        const existingPerms = await Notifications.getPermissionsAsync();
+
+        if (existingPerms.status !== 'granted') {
+            // User has not yet granted us notification permissions
+            const { status } = await Notifications.requestPermissionsAsync();
+
+            if (status !== 'granted') {
+                // Even after requesting perms, user still did not accept
+                return;
+            }
+        }
+
+        // Get the expoPushToken
+        expoPushToken = await Notifications.getExpoPushTokenAsync();
+        expoPushToken = expoPushToken.data;
+
+        return expoPushToken;
+    }
 
     return (
         <Tab.Navigator screenOptions={({ route }) => ({
