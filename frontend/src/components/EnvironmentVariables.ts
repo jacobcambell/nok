9ff1eb0968b2af37f688849d772f5c8c7aac1644
Jacob Cambell <jacob@jacobcambell.com>
2021-10-24 11:58:56 -0600

    Updated env variables

--- frontend/src/components/EnvironmentVariables.ts
+++ frontend/src/components/EnvironmentVariables.ts
@@ -1,2 +1,4 @@
-// export const API_ENDPOINT = 'http://192.168.0.108:4000';
+export const SOCKET_ENDPOINT = 'http://143.198.78.181:6000';
+// export const SOCKET_ENDPOINT = 'http://192.168.0.108:6000';
 export const API_ENDPOINT = 'http://143.198.78.181:4000';
+// export const API_ENDPOINT = 'http://192.168.0.108:4000';
