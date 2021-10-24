faf78eeb4b4fa4267290ed6d1969892fb44c717c
Jacob Cambell <jacob@jacobcambell.com>
2021-10-24 09:56:07 -0600

    Move ping and notification register to AuthContext

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -2,6 +2,9 @@ import React, { createContext, useEffect } from 'react';
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import * as SecureStore from 'expo-secure-store';
+import * as Notifications from 'expo-notifications'
+import axios from 'axios';
+import { API_ENDPOINT } from '../components/EnvironmentVariables';
 
 const firebaseApp = initializeApp({
     apiKey: "AIzaSyAnQ4G4n0kigRIap659em1tB3HnLUiL2I8",
@@ -20,9 +23,18 @@ export default function AuthProvider({ children }: { children: any }) {
     useEffect(() => {
         firebaseAuth.onAuthStateChanged((user) => {
             firebaseAuth.currentUser?.getIdToken(true)
-                .then((token) => {
+                .then(async (token) => {
                     // Save the user's idToken to SecureStore
                     SecureStore.setItemAsync('firebase_idToken', token);
+
+                    registerForPushNotificationsAsync().then((expoPushToken) => {
+                        // Ping the api with the user's idToken
+                        axios.post(`${API_ENDPOINT}/ping`, {
+                            idToken: token,
+                            expoPushToken
+                        })
+                            .catch((err) => { })
+                    })
                 })
         });
     }, []);
@@ -32,4 +44,26 @@ export default function AuthProvider({ children }: { children: any }) {
             {children}
         </AuthContext.Provider>
     );
+}
+
+async function registerForPushNotificationsAsync() {
+    let expoPushToken;
+
+    const existingPerms = await Notifications.getPermissionsAsync();
+
+    if (existingPerms.status !== 'granted') {
+        // User has not yet granted us notification permissions
+        const { status } = await Notifications.requestPermissionsAsync();
+
+        if (status !== 'granted') {
+            // Even after requesting perms, user still did not accept
+            return;
+        }
+    }
+
+    // Get the expoPushToken
+    expoPushToken = await Notifications.getExpoPushTokenAsync();
+    expoPushToken = expoPushToken.data;
+
+    return expoPushToken;
 }
